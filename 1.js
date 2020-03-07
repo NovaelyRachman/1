0a07b58f91493c99a51397c27843a1d593bfdff5
NovaelyRachman <43128781+NovaelyRachman@users.noreply.github.com>
2020-03-07 20:55:39 +0800

    Add files via upload
    Menggunakan javascript sudah di coba di https://codepen.io/pen/?editors=1111

---
+++ 1.js
@@ -0,0 +1,16 @@
+var hari = 0;
+var thari = 0;
+var iguana = 2169;
+var tguana = 0;
+
+for (var i = 0; i < 277; i++) {
+
+    if ((i - thari) == 92) {
+        tiguana = Math.round(iguana * (11.1 / 100));
+        iguana = Math.round(iguana - tiguana);
+        iguana *= 2;
+        thari = i;
+        console.log(iguana);
+
+    }
+}
